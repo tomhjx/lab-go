2d49c7c1ab1fccecb3a3d0f1b5a0355c3eed485a
Tom <tom_hejiaxi@163.com>
2023-01-25 10:47:11 +0800

    ymd

--- time/handle.go
+++ time/handle.go
@@ -2,6 +2,18 @@ package time
 
 import "time"
 
-func NextHourlyUnix() int64 {
-	return time.Now().Add(1 * time.Hour).Truncate(time.Hour).Unix()
+func HoursUnix(n time.Duration) int64 {
+	return time.Now().Add(n * time.Hour).Truncate(time.Hour).Unix()
+}
+
+func DaysUnix(n int) int64 {
+	return DaysTime(n).Unix()
+}
+
+func DaysYmd(n int) string {
+	return DaysTime(n).Format("20060102")
+}
+
+func DaysTime(n int) time.Time {
+	return time.Now().AddDate(0, 0, n)
 }
--- time/handle_test.go
+++ time/handle_test.go
@@ -4,6 +4,14 @@ import (
 	"testing"
 )
 
-func TestNextHourlyUnix(t *testing.T) {
-	t.Logf("time.Truncate: %v", NextHourlyUnix())
+func TestHoursUnix(t *testing.T) {
+	t.Logf("after 1 hour unix: %v", HoursUnix(1))
+}
+
+func TestDaysUnix(t *testing.T) {
+	t.Logf("after 1 day unix: %v", DaysUnix(1))
+}
+
+func TestDaysYmd(t *testing.T) {
+	t.Logf("after 1 day Ymd: %v", DaysYmd(1))
 }
