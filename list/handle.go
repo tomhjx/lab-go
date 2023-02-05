400ac8f6a25245c70b9028969b754ed090182959
Tom <tom_hejiaxi@163.com>
2023-02-05 22:32:30 +0800

    Shuffle

--- list/handle.go
+++ list/handle.go
@@ -1,8 +1,35 @@
 package list
 
+import (
+	"math/rand"
+	"time"
+)
+
+// 数组分片
 func ChunkInts(items []int, chunkSize int) (chunks [][]int) {
 	for chunkSize < len(items) {
 		items, chunks = items[chunkSize:], append(chunks, items[0:chunkSize:chunkSize])
 	}
 	return append(chunks, items)
+}
+
+// 数组去重
+func UniqueStrings(l []string) (ret []string) {
+	exists := map[string]bool{}
+	for _, v := range l {
+		if _, ok := exists[v]; ok {
+			continue
+		}
+		exists[v] = true
+		ret = append(ret, v)
+	}
+	return ret
+}
+
+// 随机打乱数组
+func Shuffle(l []interface{}) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	r.Shuffle(len(l), func(i, j int) {
+		l[i], l[j] = l[j], l[i]
+	})
 }
--- list/handle_test.go
+++ list/handle_test.go
@@ -9,8 +9,26 @@ func TestChunkInt(t *testing.T) {
 	for i := 0; i < 55; i++ {
 		l = append(l, i)
 	}
-	lg := ChunkInts(l, 10)
-	for _, v := range lg {
-		t.Logf("%v", v)
+
+	sizes := []int{10, 100}
+	for _, s := range sizes {
+		t.Logf("chunk by size:%v", s)
+		lg := ChunkInts(l, s)
+		for _, v := range lg {
+			t.Logf("%v", v)
+		}
 	}
+
+}
+
+func TestUniqueStrings(t *testing.T) {
+	l := []string{"a", "b", "a", "c", "b", "d"}
+	l = UniqueStrings(l)
+	t.Logf("%v", l)
+}
+
+func TestShuffle(t *testing.T) {
+	l := []interface{}{"a1", "a2", "a3", "b1", "b2", "b3"}
+	Shuffle(l)
+	t.Logf("%v", l)
 }
